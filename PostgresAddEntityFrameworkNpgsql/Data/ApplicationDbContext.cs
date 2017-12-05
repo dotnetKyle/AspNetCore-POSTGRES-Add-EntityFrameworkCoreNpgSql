320ab1fe45c47af5080880dc38d457d5a28346d6
Kyle Burrows <burozapps@gmail.com>
2017-12-04 19:26:44 -0500

    Override the SqlServer default "dbo" schema with the POSTGRES "public" schema.  Entity Framework uses "dbo" as the default.

--- PostgresAddEntityFrameworkNpgsql/Data/ApplicationDbContext.cs
+++ PostgresAddEntityFrameworkNpgsql/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace PostgresAddEntityFrameworkNpgsql.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.HasDefaultSchema("public");
+
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
