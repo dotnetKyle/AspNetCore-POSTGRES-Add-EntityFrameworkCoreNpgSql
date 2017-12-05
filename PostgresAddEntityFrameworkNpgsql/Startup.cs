589c7f8a5da55adf87ed9f7d1ad170305b4e5dfc
Kyle Burrows <burozapps@gmail.com>
2017-12-04 19:28:41 -0500

    Remove the "UseSqlServer" entity framework references and replace them with "UseNpgSql" references.

--- PostgresAddEntityFrameworkNpgsql/Startup.cs
+++ PostgresAddEntityFrameworkNpgsql/Startup.cs
@@ -26,8 +26,11 @@ namespace PostgresAddEntityFrameworkNpgsql
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services
+                .AddEntityFrameworkNpgsql()
+                .AddDbContext<ApplicationDbContext>(options =>
+                    options.UseNpgsql(Configuration["DefaultConnection"])
+                );
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
